b74ea71fa64ee3aeb25e17990f0b476391b5bb05
SHASHWAT MISHRA <85394665+SHASHWAT-MISHRA-997@users.noreply.github.com>
2024-11-17 16:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ options.headless = True
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--window-size=1920x1080')
+options.add_argument('--disable-dev-shm-usage')  # Add this line
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Functions
